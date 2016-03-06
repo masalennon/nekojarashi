8d8d3d5f64ee239e7dfcaf598c88f859ac190a5a
Masanari Miyazaki <competence.80@gmail.com>
2016-03-06 16:26:07 +0900

    impletment on click

--- src/com/example/GameApp/MyActivity.java
+++ src/com/example/GameApp/MyActivity.java
@@ -2,6 +2,9 @@ package com.example.GameApp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;//SDKからクラスを読み込むのがimport
+import android.widget.Button;
 
 public class MyActivity extends Activity {
     /**
@@ -10,6 +13,17 @@ public class MyActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main);//mainxmlを読み込む
+        // ボタンのオブジェクトを取得.
+        Button btn = (Button)findViewById(R.id.button);//ボタンオブジェクトを取得
+
+        // クリックイベントを受け取れるようにする
+        btn.setOnClickListener(new View.OnClickListener() {
+            // このメソッドがクリック毎に呼び出される
+            public void onClick(View v) {
+                // ここにクリックされたときの処理を記述
+                Log.d("test", "onclick");
+            }
+        });
     }
 }
